316ae626ce9aacee423cf5e2a7f27b6db47027fb
敏 邹 <funy_baby@outlook.com>
2017-06-07 23:56:29 +0800

    1111111

--- Common/Contact.cs
+++ Common/Contact.cs
@@ -9,6 +9,7 @@ namespace Common
     public class Contact
     {
         public string Id { get; set; }
+        public string Id2 { get; set; }
         public string Name { get; set; }
         public string PhoneNo { get; set; }
         public string EmailAddress { get; set; }
